fd0c322d451ad3a5356cc32a2fccdabca50b5a7a
Lucas Lutze <lucas.lutze@smail.inf.h-brs.de>
2022-02-03 11:23:00 +0100

    Content angelegt

--- Webseite/components/menu_navbar.js
+++ Webseite/components/menu_navbar.js
@@ -3,7 +3,7 @@ import {LitElement, html, css} from 'https://mkaul.github.io/lit/lib/lit.js';
 export class Menu_navbar extends LitElement {
   static styles = css`
   ul {
-    padding: 5px;
+    padding: 10px;
     width: 95%;
     margin: auto;
     list-style-type: none;
@@ -15,21 +15,29 @@ export class Menu_navbar extends LitElement {
     background-color: #FFFFFF;
     color: black;
     font-weight: bold;
+    font-family: sans-serif;
     border-radius: 20px;
     box-shadow: 15px;
     min-height: 25px;
-    padding: 5px;
+    padding: 10px;
     margin: 5px;
     transition-duration: 0.4s;
     justify-content: center;
   }
   button:hover{
+    border: 0.5px solid #290596;
     box-shadow: #290596 0px 4px 6px 0px;
   }
   button:focus{
     border: 0.5px solid #290596;
-    background-color: #290596;
+    box-shadow: #290596 0px 4px 6px 0px;
+    background: rgb(97,89,231);  /* https://cssgradient.io/gradient-backgrounds/ */
+    background: linear-gradient(90deg, rgba(97,89,231,1) 0%, rgba(45,45,180,1) 20%, rgba(41,5,150,1) 50%, rgba(45,45,180,1) 80%, rgba(97,89,231,1) 100%);
     color: white;
+  }
+  button:active{
+    border: 0.5px solid #290596;
+    box-shadow: #290596 0px 4px 6px 0px;
   }
 
   .direction {
@@ -40,6 +48,7 @@ export class Menu_navbar extends LitElement {
   }
   .direction button {
     display: inline;
+    min-width: 60px;
   }
 
   @media (min-width: 992px) {
