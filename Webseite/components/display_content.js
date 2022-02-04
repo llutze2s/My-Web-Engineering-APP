5a1380828de655b781ac75ad78cca3314694892f
Lucas Lutze <lucas.lutze@smail.inf.h-brs.de>
2022-02-04 10:42:58 +0100

    Main js erstellt

---
+++ Webseite/components/display_content.js
@@ -0,0 +1,12 @@
+import {LitElement, html, css} from 'https://mkaul.github.io/lit/lib/lit.js';
+
+export class Display_Content extends LitElement {
+  static styles = css``;
+
+  render() {
+
+  }
+
+  
+}
+customElements.define('display-content', Display_Content);
---
+++ Webseite/components/main.js
@@ -0,0 +1,5 @@
+async function init(){
+    content = document.getElementById("content");
+    let content_component = document.createElement("display-content");
+    content.appendChild(content_component);
+}
