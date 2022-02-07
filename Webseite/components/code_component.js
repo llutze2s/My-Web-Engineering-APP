f386764bf442cbf8fc7a9db39a358c3a386c49e4
Lucas Lutze <lucas.lutze@smail.inf.h-brs.de>
2022-02-07 12:30:32 +0100

    Code Komponente fertiggestellt mit umschaltung der Ansicht

--- Webseite/components/code_component.js
+++ Webseite/components/code_component.js
@@ -1,12 +1,37 @@
 import {LitElement, html, css, unsafeHTML} from 'https://mkaul.github.io/lit/lib/lit.js';
 
 export class Code_Component extends LitElement {
-    static styles = css``
+    static styles = css`
+        .box {
+            max-width: 100%;
+            max-height: 100%;
+            overflow: auto;
+            border-radius: 5px;
+        }
+        .code {
+            max-width: 100%;
+            max-height: 50%;
+            overflow: auto;
+            border-radius: 5px;
+        }
+        pre,code {
+            white-space: pre-wrap;
+            overflow-x: auto;
+            text-align: left;
+            tab-size: 1;
+        }
+    `
 
     static properties = {
         path: {type: String},
-        content: {type: String}
+        content: {type: String},
+        codeView: {type: Boolean}
     };
+
+    constructor() {
+        super();
+        this.codeView = false;
+    }
 
     connectedCallback() {
         super.connectedCallback();
@@ -22,16 +47,34 @@ export class Code_Component extends LitElement {
      }
     
     render() {  // https://stackoverflow.com/questions/57103073/lit-element-outputting-raw-html-from-property-string
-    return html`
-        <iframe frameborder="0" src="Webseite/content/einfuerung/snippet1.html"></iframe>
-        ${unsafeHTML(this.content)}`; 
+        if(this.codeView == false){
+            return html`
+                <div class="box" id=${this.path}>
+                    ${unsafeHTML(this.content)}
+                </div>
+                <button type="button" id=${this.path} @click=${(e) => this._press(e)}>Code Anzeigen</button>
+            `;
+        } else {
+            return html`
+                <div class="code" id=${this.path}>
+                    <pre>
+                        <code>
+                            ${this.content}
+                        </code>
+                    </pre>
+                </div>
+                <button type="button" id=${this.path} @click=${(e) => this._press(e)}>Webseite Anzeigen</button>
+            `;
+        }
     }
 
     _press(e) {
-        var code = document.getElementById(1.1).innerHTML;
-        //Html encoden
-        code.replaceAll("<","&lt;");
-        code.replaceAll(">","&gt;");
+        if( this.codeView == true){
+            this.codeView = false;
+        } else {
+            this.codeView = true;
+        }
+        
     }
 }
 customElements.define('code-component', Code_Component);
