68154d4ef5f3bda17b88197220f32d108e731ed7
Lucas Lutze <lucas.lutze@smail.inf.h-brs.de>
2022-02-17 11:02:17 +0100

    Script um Tabellenausgabe ergänzt

--- lineofcodes.py
+++ lineofcodes.py
@@ -1,6 +1,9 @@
 import os
 import glob
 
+#Für Tabellenausgabe
+from texttable import Texttable 
+
 #Alle Dateien Sammeln
 all_files= glob.glob("./**",recursive=True)
 
@@ -28,13 +31,19 @@ filter_files = [f for f in filter_files if
     "node_modules" not in f
 )]
 
+#https://stackoverflow.com/questions/9535954/printing-lists-as-tabular-data
+t = Texttable()
+t.add_row(['File', 'Line of Code'])
+
 count=0
 #Zeilen Zählen
 for file in filter_files:
     try:
         current = sum(1 for line in open(file))
+        t.add_row([file,str(current)])
         count += current
     except:
         None
 
+print(t.draw())
 print(str(count)+" Zeilen in "+ str(len(filter_files))+" Dateien." )
